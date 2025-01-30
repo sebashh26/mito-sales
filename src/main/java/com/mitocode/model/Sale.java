fbf04f418c84bcef81d9030364aaed3d1d6db8d3
sebashh26 <sebashh26@gmail.com>
2025-01-30 12:57:39 -0500

    mapping one to many, header -> details

---
+++ src/main/java/com/mitocode/model/Sale.java
@@ -0,0 +1,56 @@
+package com.mitocode.model;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.ForeignKey;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
+public class Sale {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@EqualsAndHashCode.Include
+	private Integer idSale;
+	
+	@ManyToOne
+	@JoinColumn(name="id_client", nullable = false, foreignKey = @ForeignKey(name="FK_SALE_CLIENT"))
+	private Client client;
+
+	@ManyToOne
+	@JoinColumn(name="id_user", nullable = false, foreignKey = @ForeignKey(name="FK_SALE_USER"))
+	private User user;
+	
+	@OneToMany(mappedBy = "sale", cascade = CascadeType.ALL)//atributo por el q se hace el match,RELACION DE MAESTRO DETALLE INSERTARA DE GOLPE LA VENTA Y SUS ITEMS
+	private List<SaleDetail> details;
+	
+	@Column(nullable = false)
+	private LocalDateTime dateTime;
+	
+	@Column(columnDefinition = "decimal(6,2)", nullable = false)
+	private double total;
+	
+	@Column(columnDefinition = "decimal(6,2)", nullable = false)
+	private double tx;
+	
+	@Column(nullable = false)
+	private boolean enabled;
+	
+}
---
+++ src/main/java/com/mitocode/model/SaleDetail.java
@@ -0,0 +1,45 @@
+package com.mitocode.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.ForeignKey;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
+public class SaleDetail {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@EqualsAndHashCode.Include
+	private Integer idSaleDetail;
+	
+	
+	@ManyToOne
+	@JoinColumn(name="id_sale", nullable = false, foreignKey = @ForeignKey(name="FK_DETAIL_SALE"))
+	private Sale sale;
+	
+	@ManyToOne
+	@JoinColumn(name="id_product", nullable = false, foreignKey = @ForeignKey(name="FK_DETAIL_PRODUCT"))
+	private Product product;
+	
+	@Column(nullable = false)
+	private short quantity;
+	
+	@Column(columnDefinition = "decimal(6,2)", nullable = false)
+	private double salePrice;
+	
+	@Column(columnDefinition = "decimal(6,2)", nullable = false)
+	private double discount;
+}
