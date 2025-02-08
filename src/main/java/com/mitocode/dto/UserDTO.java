fc997ceb59df23aeb5e81cd124ee0006eea2d1d7
sebashh26 <sebashh26@gmail.com>
2025-02-07 21:56:41 -0500

    use @JsonIncludeProperties,@JsonProperty,@JsonProperty,JsonIgnore

--- src/main/java/com/mitocode/dto/UserDTO.java
+++ src/main/java/com/mitocode/dto/UserDTO.java
@@ -1,5 +1,9 @@
 package com.mitocode.dto;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonIncludeProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
@@ -14,14 +18,18 @@ public class UserDTO {
 
 	private Integer idUser;
 	
+	@JsonIncludeProperties(value = {"idRole","nameRole"})//mostar propiedades del obj
 	@NotNull
 	private RoleDTO role;
 	
+	@JsonProperty(value = "user_name")//para integraciones
 	@NotNull
 	@NotEmpty
 	@Size(min = 3, max = 50)
 	private String  userName;
 	
+	@JsonProperty(access = JsonProperty.Access.WRITE_ONLY)//defino que no mas hacer con la varibale lectura o escritura
+	//@JsonIgnore
 	@NotNull
 	@NotEmpty
 	@Size(min = 3, max = 60)
