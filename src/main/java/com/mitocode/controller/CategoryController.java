79a8362045250da3705fdbf9d86d5fdac1545a16
sebashh26 <sebashh26@gmail.com>
2025-02-20 13:15:38 -0500

    @PreAuthorize in controller but isnt dinamyc, if change role in bd affected to the app

--- src/main/java/com/mitocode/controller/CategoryController.java
+++ src/main/java/com/mitocode/controller/CategoryController.java
@@ -9,6 +9,7 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -39,6 +40,7 @@ public class CategoryController {
 	@Qualifier("categoryMapper")
 	private final ModelMapper mapper;
 
+	@PreAuthorize("hasAuthority('ADMIN') or hasAuthority('USER')")
 	@GetMapping
 	public ResponseEntity<List<CategoryDTO>> readAll() throws Exception {
 
